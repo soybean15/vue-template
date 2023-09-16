6fe0fd416ff446f67d3edbfe9647fc1a3c3681b2
soybean15 <marlonpadilla1593@gmail.com>
2023-09-16 16:03:36 +0800

    initial commit

---
+++ src/components/AttendanceCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="background-color: aqua; width: 500px; height: 150px;">
+    <img  src="@/assets/selfie.webp" style="width: 300px; height: 150px"/>
+  </div>
+
+  {{ attendance.name }}
+  {{ attendance.subject }}
+  {{ attendance.time_in }}
+  {{ attendance.time_out }}
+</template>
+
+<script>
+export default {
+    props:['attendance']
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+    <div class=" flex-col">
+      <div v-for="item in attendance" :key="item.name" >
+    
+
+<AttendanceCardVue :attendance="item"/> 
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AttendanceCardVue from '@/components/AttendanceCard.vue';
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{AttendanceCardVue},
+  setup(){
+    const attendance = [
+        {name: "JEricho", subject:"Math",time_in:"5:00",time_out:"6:30"},
+        {name: "Marlon Padilla", subject:"Math",time_in:"5:00",time_out:"6:30"},
+        {name: "NEstor Samson", subject:"Math",time_in:"5:00",time_out:"6:30"},
+        {name: "Avegail Macedonio", subject:"Math",time_in:"5:00",time_out:"6:30"}
+
+    ]
+
+    return{
+
+      attendance
+ 
+
+    }
   }
+ 
 }
 </script>
+<style scoped>
+
+
+.flex{
+  display:flex;
+}
+.flex-col{
+  display:flex;
+  flex-direction: column;
+}
+</style>
